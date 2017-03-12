85e3be5f1104b642a58bf9c0e80c895c168f9673
Cpzty <Cristianpzty@gmail.com>
2017-03-12 13:29:05 -0600

    Update Desarrolladores.java
    Ahi esta el ciclo y solo falta ver que mas piden en blackboard

--- Hojaseis/src/Desarrolladores.java
+++ Hojaseis/src/Desarrolladores.java
@@ -7,80 +7,131 @@
  * @since 11 Marzo 2017
  */
 
-import java.util.HashSet;
-import java.util.TreeSet;
-import java.util.LinkedHashSet;
+
 import java.util.Scanner;
 import java.util.Set;
 
-
-
 public class Desarrolladores {
-	public static void main(String[] args) {
-		
-		// Variables
-		Set<String> MySetJAVA;
-		Set<String> MySetANDROID;
+
+	public static <E> void main(String[] args) throws IOException {
+		Set<String> MySetAndroid;
+		Set<String> MySetJava;
 		Set<String> MySetIOS;
+		String nombre;
+		boolean loop;
+		boolean keep;
 		Scanner s = new Scanner(System.in);
 		FactoryDesarrollo<String> FactoryDesarrolladores = new FactoryDesarrollo<String>();
-		boolean correr1 = true;
-		boolean correr2 = true;
 		
-		// Solicita que implementacion quiere el usuario
+		
+		
 		System.out.println("Bienvendio al programa de Desarrolladores");
+		
+		//Java
 		System.out.println("Presione 1 para usar HashSet");
+		
+		//Android
 		System.out.println("Presione 2 para usar TreeSet");
+		
+		//IOS
 		System.out.println("Presione 3 para usar LinkedHashSet");
 		int opcion = s.nextInt();
 		
-		// Se crean los tres conjuntos de la misma implementacion
-		MySetJAVA = FactoryDesarrolladores.Setcall(opcion);
-		MySetANDROID = FactoryDesarrolladores.Setcall(opcion);
-		MySetIOS = FactoryDesarrolladores.Setcall(opcion);
 		
-		// Ingresar nombres hasta que ya no quiera
-		while (correr1) {
-			System.out.println("Ingrese el nombre del Desarrollador o escriba FIN para finalizar el programa")
-			String nombre = s.nextLine();
+		MySetAndroid =FactoryDesarrolladores.Setcall(opcion);
+		MySetJava =FactoryDesarrolladores.Setcall(opcion);
+		MySetIOS =FactoryDesarrolladores.Setcall(opcion);
+		loop=true;
+		while(loop==true){
+			keep=true;
+			if(keep==true){
+			System.out.println("Presione 1 para agregar un nombre");
+			System.out.println("Presione 2 para buscar un nombre ");
+			System.out.println("Presione 3 para terminar");
+			opcion=s.nextInt();
 			
-			if (nombre.equals("FIN") || nombre.equals("fin") || nombre.equals("Fin")) {
-				correr1 = false;
-			}
-			
-			// Ingresar a que conjunto pertenece el desarrollador
-			else {
-				System.out.println("Presione 1 si es para Java, 2 si es para Andorid o 3 si es para iOS (presione 0 para seguir agregando mas desarrolladores)");
-				int numeroDesarrollo = s.nextInt();
+			if(opcion==1){
+				System.out.println("Presione 1 para agregar a Android");
+				System.out.println("Presione 2 para agregar a Java");
+				System.out.println("Presione 3 para agregar a IOS");
+				int opcion2=s.nextInt();
 				
-				// Sigue agregando a los conjuntos, aunque no se puede duplicar un desarrollador en el mismo
-				while (correr2) {
-					switch (numeroDesarrollo) {
-					case 0:
-						correr2 = false;
-					case 1:
-						MySetJAVA.add(nombre);
-						correr2 = true;
-					case 2:
-						MySetANDROID.add(nombre);
-						correr2 = true;
-					case 3:
-						MySetIOS.add(nombre);
-						correr2 = true;
-					default:
-						return null;
-					}
+				if(opcion2==1){
+					System.out.println("Android seleccionado!");
+					System.out.println("Ingresa el nombre que desees");
+					String opcion3=s.next();
+					MySetAndroid.add(opcion3);
+					opcion2=0;
 				}
-				correr1 = true;
-			}			
+				if(opcion2==2){
+					System.out.println("Java seleccionado!");
+					System.out.println("Ingresa el nombre que desees");
+					String opcion3=s.next();
+					MySetJava.add(opcion3);
+					opcion2=0;
+				}
+				
+				if(opcion2==3){
+					System.out.println("IOS seleccionado!");
+					System.out.println("Ingresa el nombre que desees");
+					String opcion3=s.next();
+					MySetIOS.add(opcion3);
+					opcion2=0;
+				}
+				
+			}
+			if(opcion==2){
+				System.out.println("Presione 1 para buscar en Android");
+				System.out.println("Presione 2 para buscar en Java");
+				System.out.println("Presione 3 para buscar en IOS");
+				int opcion2=s.nextInt();
+				
+				if(opcion2==1){
+					System.out.println("Android seleccionado!");
+					System.out.println("Ingresa el nombre que desees buscar");
+					String opcion3=s.next();
+					if(MySetAndroid.contains(opcion3)==false){
+						System.out.println(opcion3+" no se encuentra en esta lista");
+					}
+					else{
+						System.out.println(opcion3+" Esta en esta lista");
+						System.out.println("");
+					}
+					opcion2=0;
+				}
+				if(opcion2==2){
+					System.out.println("Java seleccionado!");
+					System.out.println("Ingresa el nombre que desees buscar");
+					String opcion3=s.next();
+					if(MySetJava.contains(opcion3)==false){
+						System.out.println(opcion3+" no se encuentra en esta lista");
+					}
+					else{
+						System.out.println(opcion3+" Esta en esta lista");
+						System.out.println("");
+					}
+					opcion2=0;
+				}
+				if(opcion2==3){
+					System.out.println("IOS seleccionado!");
+					System.out.println("Ingresa el nombre que desees buscar");
+					String opcion3=s.next();
+					if(MySetIOS.contains(opcion3)==false){
+						System.out.println(opcion3+" no se encuentra en esta lista");
+					}
+					else{
+						System.out.println(opcion3+" Esta en esta lista");
+						System.out.println("");
+					}
+					opcion2=0;
+				}
+			}
+			if(opcion==3){
+				loop=false;
+			}
+		
+			}
 		}
-		
-		///aqui sacar cosas
-		System.out.println(MySetJAVA);
-		System.out.println(MySetANDROID);
-		System.out.println(MySetIOS);
-		
-		
 	}
 
 }
